3d705f8168f6b5cb7cc0121deaaaa0fb396954ca
Fupi <fupicat@gmail.com>
2019-12-17 19:59:49 -0300

    Made running

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,14 +2,16 @@ extends KinematicBody2D
 
 var move = Vector2(0, 0)
 const SPEED = 400
+const RUNADD = 400
 const SLIP = 0.20
 
 func _physics_process(delta):
     z_index = position.y
+    var run = RUNADD * int(Input.is_action_pressed("run"))
     var ydir = int(Input.is_action_pressed("down")) - int(Input.is_action_pressed("up"))
-    move.y = lerp(move.y, SPEED * ydir, SLIP)
+    move.y = lerp(move.y, (SPEED + run) * ydir, SLIP)
     
     var xdir = int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left"))
-    move.x = lerp(move.x, SPEED * xdir, SLIP)
+    move.x = lerp(move.x, (SPEED + run) * xdir, SLIP)
     
     move_and_slide(move)
