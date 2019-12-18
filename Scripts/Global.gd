e2e84bf45a45b8d71a0581126f6354f91dba8b8a
Fupi <fupicat@gmail.com>
2019-12-18 16:15:22 -0300

    Moved player + global.gd

---
+++ Scripts/Global.gd
@@ -0,0 +1,5 @@
+extends Node
+
+var day = 1
+var month = 1
+var year = 1
--- Scripts/Places/House.gd
+++ Scripts/Places/House.gd
@@ -1,2 +1,6 @@
 extends Node2D
 
+func _ready():
+    $Player/Camera2D.limit_left = -3500
+    $Player/Camera2D.limit_right = 3500
+    $Player/Camera2D.limit_bottom = 1850
