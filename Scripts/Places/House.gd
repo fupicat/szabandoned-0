a1ef6ccf6c681695224d0eaab5aab75ba2250fd9
Fupi <fupicat@gmail.com>
2019-12-19 17:32:35 -0300

    Moved everything for NinePatchRect grounds

--- Scripts/Places/House.gd
+++ Scripts/Places/House.gd
@@ -3,4 +3,4 @@ extends Node2D
 func _ready():
     $Player/Camera2D.limit_left = -3500
     $Player/Camera2D.limit_right = 3500
-    $Player/Camera2D.limit_bottom = 1850
+    $Player/Camera2D.limit_bottom = 4720
