d570528aa14201b214154dbf682edb21d74c8a8c
Fupi <fupicat@gmail.com>
2019-12-24 12:17:55 -0300

    Disabled collisions during editing to avoid getting stuck

--- Scripts/BehindDisappear.gd
+++ Scripts/BehindDisappear.gd
@@ -2,9 +2,9 @@ extends Area2D
 
 func _on_body_entered(body):
     var player = get_parent().get_parent().get_node("Player")
-    if body == player and player.z_index < get_parent().z_index and !get_parent().get_parent().edit_mode:
+    if body == player and player.z_index < get_parent().z_index and !get_parent().move:
         get_parent().modulate.a = 0.54902
 
 func _on_body_exited(body):
-    if body == get_parent().get_parent().get_node("Player") and !get_parent().get_parent().edit_mode:
+    if body == get_parent().get_parent().get_node("Player") and !get_parent().move:
         get_parent().modulate.a = 1
--- Scripts/Places/House.gd
+++ Scripts/Places/House.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+#Talvez: Criar area2D com a shape do obj e ver se overlapa. Se overlapeando.x > eu.x, eu.x -= 1 até não overlapear sacou?
+
 var old_xy = Vector2(0, 0)
 var old_col = null
 var edit_mode = false
@@ -29,7 +31,7 @@ func switch_edit_mode(): # Returns true if switched.
         for node in get_tree().get_nodes_in_group("Interactable"):
             if node.move:
                 node.move = false
-                node.get_node("Col").disabled = false
+            node.get_node("Col").disabled = false
         edit_mode = false
         $Player.global_position = old_xy
         $Player/CollisionShape2D.shape = old_col
@@ -40,6 +42,8 @@ func switch_edit_mode(): # Returns true if switched.
         old_xy = $Player.global_position
         old_col = $Player/CollisionShape2D.shape
         $Player.hide()
+        for node in get_tree().get_nodes_in_group("Interactable"):
+            node.get_node("Col").disabled = true
     return true
 
 func add_placeable(var scene):
