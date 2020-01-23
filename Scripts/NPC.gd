097691b0ad4272c001793cb14153d0f7f4f6a0f2
Fupi <fupicat@gmail.com>
2020-01-23 15:42:09 -0300

    NPC stops if player is near

--- Scripts/NPC.gd
+++ Scripts/NPC.gd
@@ -4,6 +4,8 @@ var move = Vector2(0, 0)
 var speed = 500
 const SLIP = 0.20
 
+var on_me = false
+
 var target = null
 var can_walk = true
 
@@ -18,13 +20,12 @@ enum BEHAVE {
 
 func _ready():
     randomize()
+    update_z()
     if behavior == BEHAVE.wander:
         $Wander.start(rand_range(0, 3))
 
 func _physics_process(delta):
-    if target != null and can_walk:
-        #warning-ignore:NARROWING_CONVERSION
-        z_index = global_position.y / 10
+    update_z()
     
     if target != null and can_walk:
         can_walk = true
@@ -47,10 +48,7 @@ func anim_walk():
         if abs(move.x) < 30 and abs(move.y) < 30:
             $Scrat/Anim.play("Idle")
         else:
-            if Input.is_action_pressed('run'):
-                $Scrat/Anim.play('Run')
-            else:
-                $Scrat/Anim.play("Walk")
+            $Scrat/Anim.play("Walk")
 
 func animation(var name):
     can_walk = false
@@ -61,3 +59,26 @@ func _on_Wander_timeout():
     if behavior == BEHAVE.wander:
         target = Vector2(global_position.x + rand_range(-(speed * 3), speed * 3), global_position.y + rand_range(-(speed * 3), speed * 3))
         $Wander.start(rand_range(0, 3))
+
+func _on_Interact_body_entered(body):
+    if body == get_parent().get_node('Player'):
+        on_me = true
+        if behavior == BEHAVE.wander:
+            behavior = BEHAVE.stop
+            can_walk = false
+
+func _on_Interact_body_exited(body):
+    if body == get_parent().get_node('Player'):
+        on_me = false
+        if behavior == BEHAVE.stop:
+            behavior = BEHAVE.wander
+            can_walk = true
+            $Wander.start(rand_range(0, 3))
+
+func update_z():
+    #warning-ignore:NARROWING_CONVERSION
+    z_index = global_position.y / 10
+    if get_parent().get_node('Player').global_position.y > global_position.y:
+        z_index -= 1
+    else:
+        z_index += 1
